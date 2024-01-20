67c3539f31b3112ae217a0f8c5347f71d581b3d9
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-01-20 16:55:46 +0000

    footer 1.0

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,39 +1,16 @@
 import React, { Component } from "react";
-import Modal from 'bootstrap/js/dist/modal'
-const modal = new Modal(document.getElementById('myModal'))
 
 export const Footer = () => (
-<footer class="bg-dark text-white py-4">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        <h5>Divi Movies</h5>
-        <p>EXPERIENCE MOVIES</p>
-        <button class="btn btn-primary">SEE ALL OFFERS AND PROMOTIONS</button>
-      </div>
-      <div class="col-md-3">
-        <h5>ABOUT US</h5>
-        <p>Nulla porttitor accumsan tincidunt. Donec sollicitudin molestie malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur non nulla sit amet nisl!</p>
-      </div>
-      <div class="col-md-3">
-        <h5>MOVIES</h5>
-        <ul class="list-unstyled mb-0">
-          <li><a href="#" class="text-reset">Lorem ipsum dolor sit amet</a></li>
-          <li><a href="#" class="text-reset">Consectetur adipiscing elit</a></li>
-          <li><a href="#" class="text-reset">Sed do eiusmod tempor</a></li>
-          <li><a href="#" class="text-reset">Incididunt ut labore</a></li>
-        </ul>
-      </div>
-      <div class="col-md-3">
-        <h5>INFO</h5>
-        <ul class="list-unstyled mb-0">
-          <li><a href="#" class="text-reset">Lorem ipsum dolor sit amet</a></li>
-          <li><a href="#" class="text-reset">Consectetur adipiscing elit</a></li>
-          <li><a href="#" class="text-reset">Sed do eiusmod tempor</a></li>
-          <li><a href="#" class="text-reset">Incididunt ut labore</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</footer>
-)
+	<footer className="footer bg-dark text-white mt-5 py-4">
+		<button className="btn btn-primary d-block mx-auto">Check the Context in action</button>
+		<div className="container">
+		<div className="row">
+				<div className="col-md-3">
+					<h5>CineFimls</h5>
+					<p>Pendiente Descripción</p>
+					<p>Expirience Movie</p>
+				</div>
+		</div>
+		</div>
+	</footer>
+);
