6abb5657a3b20ca182cce2e485a858687b83284d
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-01-20 15:47:32 +0000

    footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+<footer class="bg-dark text-white py-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <h5>Divi Movies</h5>
+        <p>EXPERIENCE MOVIES</p>
+        <button class="btn btn-primary">SEE ALL OFFERS AND PROMOTIONS</button>
+      </div>
+      <div class="col-md-3">
+        <h5>ABOUT US</h5>
+        <p>Nulla porttitor accumsan tincidunt. Donec sollicitudin molestie malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur non nulla sit amet nisl!</p>
+      </div>
+      <div class="col-md-3">
+        <h5>MOVIES</h5>
+        <ul class="list-unstyled mb-0">
+          <li><a href="#" class="text-reset">Lorem ipsum dolor sit amet</a></li>
+          <li><a href="#" class="text-reset">Consectetur adipiscing elit</a></li>
+          <li><a href="#" class="text-reset">Sed do eiusmod tempor</a></li>
+          <li><a href="#" class="text-reset">Incididunt ut labore</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h5>INFO</h5>
+        <ul class="list-unstyled mb-0">
+          <li><a href="#" class="text-reset">Lorem ipsum dolor sit amet</a></li>
+          <li><a href="#" class="text-reset">Consectetur adipiscing elit</a></li>
+          <li><a href="#" class="text-reset">Sed do eiusmod tempor</a></li>
+          <li><a href="#" class="text-reset">Incididunt ut labore</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+)
