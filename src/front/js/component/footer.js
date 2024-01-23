19d82e83f4131fb853ef6c253f97c71a915b0c00
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-01-23 02:02:20 +0000

    Footer colummna 2

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,13 +4,18 @@ export const Footer = () => (
 	<footer className="footer bg-dark text-white mt-5 py-4">
 		<button className="btn btn-primary d-block mx-auto">Check the Context in action</button>
 		<div className="container">
-		<div className="row">
+			<div className="row">
 				<div className="col-md-3">
-					<h5>CineFimls</h5>
-					<p>Pendiente Descripción</p>
+					<h5>Pendiente Nombre</h5>
+					<p>Pendiente Descripción</p>
 					<p>Expirience Movie</p>
+					<button className="btn btn-primary">SEE ALL OFFERS AND PROMOTIONS</button>
 				</div>
-		</div>
+				<div className="col-md-3">
+					<h5>ABOUT US</h5>
+					<p>Nulla consequat massa vitae enim. Nullam ac tortor vitae purus faucibus</p>
+				</div>
+			</div>
 		</div>
 	</footer>
 );
