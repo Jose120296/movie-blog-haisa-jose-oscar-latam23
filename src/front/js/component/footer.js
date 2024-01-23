c79ffb17b0b22a3edb386a621a5449b133f72fdf
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-01-23 02:24:00 +0000

    Footer completo

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer bg-dark text-white mt-5 py-4">
-		<button className="btn btn-primary d-block mx-auto">Check the Context in action</button>
-		<div className="container">
-			<div className="row">
-				<div className="col-md-3">
-					<h5>Pendiente Nombre</h5>
-					<p>Pendiente Descripción</p>
-					<p>Expirience Movie</p>
-					<button className="btn btn-primary">SEE ALL OFFERS AND PROMOTIONS</button>
-				</div>
-				<div className="col-md-3">
-					<h5>ABOUT US</h5>
-					<p>Nulla consequat massa vitae enim. Nullam ac tortor vitae purus faucibus</p>
-				</div>
-			</div>
+	<footer className="footer bg-dark text-white mt-5 py-4"> .
+	<button className="btn btn-primary d-block mx-auto">Check the Context in action</button>
+	<div className="container">
+	  <div className="row">
+		<div className="col-md-3">
+		  <h5>Pendiente Nombre</h5>
+		  <p>Pendiente Descripción</p>
+		  <p>Expirience Movie</p>
+		  <button className="btn btn-primary">SEE ALL OFFERS AND PROMOTIONS</button>
 		</div>
-	</footer>
+		<div className="col-md-3">
+		  <h5>ABOUT US</h5>
+		  <p>Nulla consequat massa vitae enim. Nullam ac tortor vitae purus faucibus</p>
+		</div>
+		<div className="col-md-3">
+		  <h5>Movies</h5>
+		  <ul className="col-md-3">
+			<li><a href="#" className="text-reset">Lorem impsum dolor sit amet</a></li>
+			<li><a href="#" className="text-reset">Consector adispicing elit</a></li>
+			<li><a href="#" className="text-reset">Sed do eiusmod tempor</a></li>
+			<li><a href="#" className="text-reset">Incididunt ut labore</a></li>
+		  </ul>
+		</div>
+		<div className="col-md-3">
+		  <h5>INFO</h5>
+		  <ul className="list-unstyled mb-0">
+			<li><a href="#" className="text-reset">Lorem impsum dolor sit amet</a></li>
+			<li><a href="#" className="text-reset">Consector adispicing elit</a></li>
+			<li><a href="#" className="text-reset">Sed do eiusmod tempor</a></li>
+			<li><a href="#" className="text-reset">Incididunt ut labore</a></li>
+		  </ul>
+		</div>
+	  </div>
+	</div>
+  </footer>
 );
