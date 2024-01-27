0c684530821e602e175340bf076a6b618d328f83
ozzinhotv <142857305+ozzinhotv@users.noreply.github.com>
2024-01-27 03:17:05 +0000

    cambios de oscar

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,34 +1,77 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
-import { useNavigate } from "react-router-dom";
-import 'bootstrap/dist/js/bootstrap.min.js'
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { Link, useNavigate } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../../styles/home.css";
 
 export const Signup = () => {
-    const {store, actions} = useContext(Context);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    
-    const handleClick = () => {
-        actions.signup (email, password);
-    }
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleLoginClick = () => {
-        navigate('/login');
-      };
-    
-    return (
-        <div className="text-center mt-5">
-            <h1> Please Sign Up</h1>
-            <div>
-                <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button onClick={handleClick}>Sign Up</button>
-                <p className="mt-3">Have an account? </p>
-                <button className="btn btn-primary" onClick={handleLoginClick}>Login</button> 
+  const handleClick = () => {
+    actions.signup(email, password);
+  };
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    actions.getMessage();
+  }, []);
+
+  if (store.token && store.token !== "" && store.token !== undefined) {
+    navigate("/");
+  }
+
+  return (
+    <div className="home-container">
+      <div className="container">
+        <div className="row justify-content-center align-items-center h-100">
+          <div className="col-md-6">
+            <div className="card shadow">
+              <div className="card-body text-center">
+                <h1 className="mb-4">Please Sign Up</h1>
+                {store.token && store.token !== "" && store.token !== undefined ? (
+                  <p>You are logged in with this token: {store.token}</p>
+                ) : (
+                  <div className="mt-4">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                    <button className="btn btn-primary btn-block mb-3" onClick={handleClick}>
+                      Sign Up
+                    </button>
+                    <p>Already have an account?</p>
+                    <Link to="/login">
+                      <button className="btn btn-primary btn-block" onClick={handleLoginClick}>
+                        Login
+                      </button>
+                    </Link>
+                  </div>
+                )}
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
