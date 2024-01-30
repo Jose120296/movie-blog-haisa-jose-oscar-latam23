a30dca60786e38159e4201621e1415a4766acb43
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-01-30 03:04:27 +0000

    Se cambio el nombre del boton a logout

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -20,7 +20,7 @@ export const Navbar = () => {
         </Link>
         <div className="ml-auto">  
           {!store.token ? (
-            <Link to="/login">
+            <Link to="/Logout">
               <button className="btn btn-danger">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
                  <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
