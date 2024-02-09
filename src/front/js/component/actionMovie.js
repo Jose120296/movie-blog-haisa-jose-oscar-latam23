90f3e664dc77c3270ea8d3b90e6f233a97d4391c
ozzinhotv <142857305+ozzinhotv@users.noreply.github.com>
2024-02-09 23:55:29 +0000

    haciendo commit, sin cambios relevante

--- src/front/js/component/actionMovie.js
+++ src/front/js/component/actionMovie.js
@@ -3,6 +3,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
+import { Card } from "./card"
 
 export const ActionMovies = () => {
   const { store, actions } = useContext(Context);
@@ -30,6 +31,23 @@ export const ActionMovies = () => {
     }));
   };
 
+  const movieDate = new Date(movie.release_date)
+  const mesesDelAño = [
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre"
+    ]
+    const movieCardDate = `${movieDate.getDate()} de ${mesesDelAño[movieDate.getMonth()]} del ${movieDate.getFullYear()} `
+
   if (!actionMovies || actionMovies.length === 0) {
     console.log("No hay datos de películas de acción disponibles");
     return <div>No hay películas de acción disponibles</div>;
@@ -46,49 +64,15 @@ export const ActionMovies = () => {
         </Link>
       </div>
       <div className="row flex-nowrap overflow-auto">
-        {actionMovies.map((movie, index) => (
-          <div className="col" style={{ marginRight: "10px", marginBottom: "10px" }} key={index}>
-            <div className="card h-100" style={{ width: "18rem" }}>
-              <img
-                src={movie.poster}
-                className="card-img-top"
-                alt="Película"
-                style={{ objectFit: "cover", height: "400px", width:"100%" }}
-              />
-              <div className="card-body d-flex flex-column">
-                <h5 className="card-title">{movie.title}</h5>
-                <p className="card-text">{movie.genre}</p>
-                <p className="card-text">{movie.length} min</p>
-                <p className="card-text">{movie.release_date}</p>
-                <div className="rating">
-                  {[1, 2, 3, 4, 5].map((value) => (
-                    <span
-                      key={value}
-                      className={`star ${movieRatings[movie.id] && movieRatings[movie.id] >= value ? "selected" : ""}`}
-                      onClick={() => handleRatingChange(movie.id, value)}
-                    >
-                      &#9733; 
-                    </span>
-                  ))}
-                </div>
-                <div className="d-flex justify-content-between mt-auto">
-                  <Link to={`/movies/${movie.id}`} className="btn btn-danger">
-                    Ver detalles
-                  </Link>
-                  <div>
-                    <button className="btn btn-danger me-2">
-                      <i className="fa-solid fa-star"></i>
-                    </button>
-                    <button className="btn btn-danger">
-                      <i className="fa-solid fa-clock"></i>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
+        {actionMovies.map((movie, index) => {
+          
+          return(
+          <Card movie={movie} key={index}/> 
+        )})}
+        
       </div>
     </div>
   );
 };
+
+//{actionMovies.map((movie, index) => (
