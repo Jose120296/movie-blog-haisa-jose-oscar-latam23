fd19ba8ba39d10d4931ca071493f48e78d51e875
Haisa Reyes <130813009+HaisaeReyes@users.noreply.github.com>
2024-02-10 02:10:57 +0000

    traer camos de nuevo Haisa

--- src/front/js/pages/allComedyMovies.js
+++ src/front/js/pages/allComedyMovies.js
@@ -12,7 +12,7 @@ export const GetComedyMovies = () => {
   useEffect(() => {
     const fetchComedyMovies = async () => {
       try {
-        const response = await fetch(store.API_URL + "api/movies");
+        const response = await fetch(store.API_URL + "/api/movies");
         const data = await response.json();
         actions.setMovies(data);
         setComedyMovies(data.filter(movie => movie.genre.includes("Comedy")));
