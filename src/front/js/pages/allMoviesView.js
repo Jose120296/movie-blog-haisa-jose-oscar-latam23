98781504efe9ab61800f26607b8bd53b92f70ff2
ozzinhotv <142857305+ozzinhotv@users.noreply.github.com>
2024-02-13 04:52:37 +0000

    arreglando fechas del card-horizontal

--- src/front/js/pages/allMoviesView.js
+++ src/front/js/pages/allMoviesView.js
@@ -9,7 +9,6 @@ export const GetGenresMovies = () => {
   const [moviesPerPage] = useState(9);
   const [movieRatings, setMovieRatings] = useState({});
 
-
   useEffect(() => {
     const fetchMovies = async () => {
       try {
@@ -37,11 +36,6 @@ export const GetGenresMovies = () => {
     return <div>No hay películas disponibles</div>;
   }
 
-  // Obtener el índice del último y primer elemento de cada página
-  const indexOfLastMovie = currentPage * moviesPerPage;
-  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
-  const currentMovies = store.movies.slice(indexOfFirstMovie, indexOfLastMovie);
-
   // Cambiar de página
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -56,63 +50,71 @@ export const GetGenresMovies = () => {
         </Link>
       </div>
       <div className="row card-container">
-        {currentMovies.map((movie, index) => (
-          <div className="col-md-4 mb-4" style={{}} key={index}>
-            <div className="card horizontal" style={{ width: "100%", backgroundColor: "#5576B8" }}>
-              <div className="row no-gutters">
-                <div className="col">
-                  <img
-                    src={movie.poster}
-                    className="card-img"
-                    alt="Película"
-                    style={{ objectFit: "cover", width: "100%", height: "100%" }}
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h2 className="card-title">
-                      <strong>{movie.title}</strong>
-                    </h2>
-                    <p className="card-text">
-                      <strong>Genre:</strong> {movie.genre}
-                    </p>
-                    <p className="card-text">
-                      <strong>Release Date:</strong> {movie.release_date}
-                    </p>
-                    <p className="card-text horizontal">{movie.description}</p>
-                    <p className="card-text">
-                      <strong>Length:</strong> {movie.length} min
-                    </p>
-                    <div className="rating">
-                      {[1, 2, 3, 4, 5].map((value) => (
-                        <span
-                          key={value}
-                          className={`star ${movieRatings[movie.id] && movieRatings[movie.id] >= value ? "selected" : ""}`}
-                          onClick={() => handleRatingChange(movie.id, value)}
-                        >
-                          &#9733; 
-                        </span>
-                      ))}
-                    </div>
-                    <div className="d-flex justify-content-between mt-auto">
-                      <Link to={`/movies/${movie.id}`} className="btn btn-warning">
-                        View details
-                      </Link>
-                      <div>
-                        <button className="btn btn-warning me-2">
-                          <i className="fa-solid fa-star"></i>
-                        </button>
-                        <button className="btn btn-warning">
-                          <i className="fa-solid fa-clock"></i>
-                        </button>
+        {store.movies.slice((currentPage - 1) * moviesPerPage, currentPage * moviesPerPage).map((movie, index) => {
+          const movieDate = new Date(movie.release_date);
+          const mesesDelAño = [
+            "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+          ];
+          const movieCardDate = `${movieDate.getDate()} de ${mesesDelAño[movieDate.getMonth()]} del ${movieDate.getFullYear()}`;
+          return (
+            <div className="col-md-4 mb-4" style={{}} key={index}>
+              <div className="card horizontal" style={{ width: "100%", backgroundColor: "#5576B8" }}>
+                <div className="row no-gutters">
+                  <div className="col">
+                    <img
+                      src={movie.poster}
+                      className="card-img"
+                      alt="Película"
+                      style={{ objectFit: "cover", width: "100%", height: "100%" }}
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h2 className="card-title">
+                        <strong>{movie.title}</strong>
+                      </h2>
+                      <p className="card-text">
+                        <strong>Genre:</strong> {movie.genre}
+                      </p>
+                      <p className="card-text">
+                        <strong>Release Date:</strong> {movieCardDate}
+                      </p>
+                      <p className="card-text horizontal">{movie.description}</p>
+                      <p className="card-text">
+                        <strong>Length:</strong> {movie.length} min
+                      </p>
+                      <div className="rating">
+                        {[1, 2, 3, 4, 5].map((value) => (
+                          <span
+                            key={value}
+                            className={`star ${movieRatings[movie.id] && movieRatings[movie.id] >= value ? "selected" : ""}`}
+                            onClick={() => handleRatingChange(movie.id, value)}
+                          >
+                            &#9733; 
+                          </span>
+                        ))}
+                      </div>
+                      <div className="d-flex justify-content-between mt-auto">
+                        <Link to={`/movies/${movie.id}`} className="btn btn-warning">
+                          View details
+                        </Link>
+                        <div>
+                          <button className="btn btn-warning me-2">
+                            <i className="fa-solid fa-star"></i>
+                          </button>
+                          <button className="btn btn-warning">
+                            <i className="fa-solid fa-clock"></i>
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       {/* Pagination */}
@@ -137,8 +139,6 @@ export const GetGenresMovies = () => {
           </li>
         </ul>
       </nav>
-
-      
     </div>
   );
 };
